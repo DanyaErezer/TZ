5189601ee4c839b6ecbf08ec93c2e9e014fcd6c9
DanyaErezer <pavlov.shura12@gmail.com>
2025-03-12 15:27:50 +0700

    заполнение ProductControllers

--- app/Http/Controllers/ProductControllers.php
+++ app/Http/Controllers/ProductControllers.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductControllers extends Controller
@@ -11,7 +13,8 @@ class ProductControllers extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::with('category')->get();
+            return view('products.index', compact('products'));
     }
 
     /**
@@ -19,7 +22,8 @@ class ProductControllers extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+            return view('products.create', compact('categories'));
     }
 
     /**
@@ -27,38 +31,59 @@ class ProductControllers extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'nullable|string',
+        ]);
+
+        Product::create($request->all());
+
+        return redirect()->route('products.index')->with('success', 'Товар успешно добавлен!');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+        return view('products.show', compact('product'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Product $product)
     {
-        //
+        $categories = Category::all();
+            return view('products.edit', compact('product', 'categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Product $product)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'price' => 'required|numeric|min:0',
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'nullable|string',
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')->with('success', 'Товар успешно обновлен!');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('product.index')->with('success', 'Товар успешно удален!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderControllers;
+use App\Http\Controllers\ProductControllers;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('products', ProductControllers::class);
+Route::resource('orders', OrderControllers::class);
